a043a132e207d1abcd6c87c4a5bd748a92fb90c4
Drastic_Coder <deepbansodeofficial4205@gmail.com>
2022-09-03 21:34:31 +0530

    Add project section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,6 +5,24 @@ export default function Project() {
   return (
     <div className='page'>
       <h1 className='neonText'>Personal {'{Projects}'}</h1>
+      <div className="project">
+        <h2>sample project</h2>
+        <li>Sample info</li>
+        <li>Sample info</li>
+        <li>Sample info</li>
+      </div>
+      <div className="project">
+        <h2>sample project</h2>
+        <li>Sample info</li>
+        <li>Sample info</li>
+        <li>Sample info</li>
+      </div>
+      <div className="project">
+        <h2>sample project</h2>
+        <li>Sample info</li>
+        <li>Sample info</li>
+        <li>Sample info</li>
+      </div>
     </div>
   )
 }
